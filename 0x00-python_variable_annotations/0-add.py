dada4e112794c267b1716c2e8b22c2371dc70691
Abdirahman <abdirahmanabdi.ma@gmail.com>
2024-05-03 10:48:27 +0300

    new

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,6 +2,6 @@
 """Module that contains a single function that adds two integers."""
 
 
-def add(a: float, b:float) -> float:
+def add(a: float, b: float) -> float:
     """ Add two integers together."""
     return a + b
