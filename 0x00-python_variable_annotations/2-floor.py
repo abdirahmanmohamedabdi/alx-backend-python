533090c9a1ba05050a7ac8031b31f5f3211346de
Abdirahman <abdirahmanabdi.ma@gmail.com>
2024-05-03 10:38:36 +0300

    new

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Contains a single function that calculates the floor of a number."""
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of a number."""
+    return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/sur/bin/env python3
+"""Contains a function that returns a string representation of a float."""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of a float
+    Args:
+        n (float): The float to represent as a string
+    Returns:
+        A string representation of the float
+    """
+    return str(n)
