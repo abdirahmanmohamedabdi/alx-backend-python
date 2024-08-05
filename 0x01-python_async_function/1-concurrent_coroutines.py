a350bf4f86ddc9727ff492cebe2488d0073d98e4
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-08-05 22:16:38 +0300

    new

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""import wait_random from 0-basic_async_syntax and measure_time from 2-measure_runtime"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ let's execute multiple coroutines at the same time with async"""
+    delays: List[float] = []
+    all_delays: List[float] = []
+    for i in range(n):
+        delays.append(wait_random(max_delay))
+    for delay in asyncio.as_completed(delays):
+        earliest_result = await delay
+        all_delays.append(earliest_result)
+    return all_delays 
