50b143f7f99e99fac7f3c4e5c0379ab4a5b78091
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-08-06 11:29:49 +0300

    new

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/enb python3
+""" The basic of async  """
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ Wait for a random delay """
+  random_float = random.uniform(0, max_delay)
+  await asyncio.sleep(random_float)
+    return random_float
